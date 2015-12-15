61ca751aedaf253b687c76c86dca9697aa4fdcde
BattleShipPark <battleship.park@gmail.com>
2015-12-15 18:34:41 +0900

    Apply Fullscreen theme to FileActivity

--- app/src/main/java/com/battleshippark/bsp_gallery/activity/file/FileActivity.java
+++ app/src/main/java/com/battleshippark/bsp_gallery/activity/file/FileActivity.java
@@ -1,13 +1,16 @@
 package com.battleshippark.bsp_gallery.activity.file;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.battleshippark.bsp_gallery.Events;
 import com.battleshippark.bsp_gallery.R;
@@ -31,6 +34,9 @@ public class FileActivity extends AppCompatActivity implements FragmentAccessibl
     private FileActivityModel model;
 
     /* View */
+    @Bind(R.id.rootView)
+    View rootView;
+
     @Bind(R.id.viewpager)
     ViewPager viewPager;
 
@@ -163,6 +169,14 @@ public class FileActivity extends AppCompatActivity implements FragmentAccessibl
     }
 
     private void initUI() {
+        hideSystemUI();
         viewPager.setAdapter(adapter);
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private void hideSystemUI() {
+        rootView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
     }
 }
