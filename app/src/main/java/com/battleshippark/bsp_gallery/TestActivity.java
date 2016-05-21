5635d9f743106f95b84b91514c0444004019dcf4
BattleShipPark <battleship.park@gmail.com>
2016-05-22 05:15:58 +0900

    remove redundant files

--- app/src/main/java/com/battleshippark/bsp_gallery/TestActivity.java
+++ app/src/main/java/com/battleshippark/bsp_gallery/TestActivity.java
@@ -2,12 +2,8 @@ package com.battleshippark.bsp_gallery;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-
-import com.battleshippark.bsp_gallery.test.MediaControllerTest;
 
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  */
@@ -17,10 +13,5 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         ButterKnife.bind(this);
-    }
-
-    @OnClick(R.id.mediaControllerTest)
-    public void onClickMediaControllerTest(View v) {
-        MediaControllerTest.디렉토리_목록(this);
     }
 }
